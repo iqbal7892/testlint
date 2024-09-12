a6103e394f8d750314b9263f787ca24ad8c9289f
unknown <iqbal.khan78926@gmail.com>
2024-09-12 13:13:19 +0500

    Dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   const fname = "Iqbal";
+  const lname = "Iqbal";
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -21,7 +22,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>{fname}</li>
+          <li>{fname} {lname}</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
