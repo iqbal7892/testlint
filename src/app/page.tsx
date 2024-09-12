d9bb642604588c28d5ca841125d763f65c4d35c4
unknown <iqbal.khan78926@gmail.com>
2024-09-12 14:28:06 +0500

    add full name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>{fname}</li>
+          <li>{fname} {fullname}</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
